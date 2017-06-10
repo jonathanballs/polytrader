426cef02d114a38d6e94d363335505831c1df232
Jonathan Balls <j.balls@btopenworld.com>
2017-06-10 23:40:04 +0100

    Basic pie chart of crypto balance

---
+++ static/js/portfolio.js
@@ -0,0 +1,29 @@
+// Portfolio.js
+// Code for charting 
+
+var ctx = document.getElementById("portfolio-pie")
+
+var data = {
+    datasets: [{
+        data: cryptoBalances.map((x) => x.btcValue),
+        backgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+        ],
+        hoverBackgroundColor: [
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
+        ]
+    }],
+
+    // These labels appear in the legend and in the tooltips when hovering different arcs
+    labels: cryptoBalances.map((x) => x.currency)
+};
+
+var portfolioPieChart = new Chart(ctx, {
+  type: 'pie',
+  data: data,
+  options: {}
+})
