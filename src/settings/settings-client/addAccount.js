32fc14e0e5ca1735798e2af2d3c7761a0b4f40b6
Jonathan Balls <jonathanballs@protonmail.com>
2017-10-22 16:40:13 +0100

    Different button states depending on form submission status

--- src/settings/settings-client/addAccount.js
+++ src/settings/settings-client/addAccount.js
@@ -6,30 +6,34 @@ import { Modal, Button, Carousel, CarouselItem } from 'reactstrap'
 export default class AddAccount extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { showModal: false, activeSlide: 0, currentAccountForm: 'poloniex' }
+    this.state = {
+      showModal: false,
+      activeSlide: 0,
+      currentAccountForm: 'poloniex',
+      submissionStatus: 'none' // none, loading, success or failure
+    }
   }
 
   accountForms = [
     {
       service: 'poloniex',
       formFields: [
-        { name: 'apiKey', description: 'API Key', placeholder: 'Poloniex API Key'},
+        { name: 'apiKey', description: 'API Key', placeholder: 'Poloniex API Key' },
         { name: 'apiSecret', description: 'API Secret', placeholder: 'Poloniex API Secret' },]
     },
     {
       service: 'bittrex',
       formFields: [
-        { name: 'apiKey', description: 'API Key', placeholder: 'Bittrex API Key'},
+        { name: 'apiKey', description: 'API Key', placeholder: 'Bittrex API Key' },
         { name: 'apiSecret', description: 'API Secret', placeholder: 'Bittrex API Secret' },]
     },
     {
       service: 'ethereum wallet',
-      formFields: [{ name: 'walletAddress', description: 'Address', placeholder: 'Ethereum Wallet Address'}]
+      formFields: [{ name: 'walletAddress', description: 'Address', placeholder: 'Ethereum Wallet Address' }]
     }
   ]
 
-
-  toggle = () => {
+  toggleModal = () => {
     this.setState({
       showModal: !this.state.showModal,
       activeSlide: 0
@@ -37,20 +41,27 @@ export default class AddAccount extends React.Component {
   }
 
   goToSlide = (slideNum) => {
-    this.setState({activeSlide: 0});
+    this.setState({ activeSlide: slideNum });
   }
 
-  // Creatse a account form for 
+  submitAccountForm = () => {
+    var formFields = this.accountForms.filter(f => f.service == this.state.currentAccountForm)[0]
+    this.setState({ submissionStatus: 'loading' })
+
+    setTimeout(_ => this.setState({ submissionStatus: 'success' }), 3000)
+
+  }
+
   makeAccountForm = (account) => {
     var accountFormDesc = this.accountForms.filter(a => a.service == account)[0]
     var accountFormFields = accountFormDesc.formFields.map((ff, i) => {
       return (
-      <div key={i} className="form-group row">
-        <label className="col-md-2 col-form-label" htmlFor={ff.name}>{ff.description}</label>
-        <div className="col-md-10">
-          <input className="form-control" id="poloniexApiKey" type="text" name={ff.name} placeholder={ff.placeholder} required />
+        <div key={i} className="form-group row">
+          <label className="col-md-2 col-form-label" htmlFor={ff.name}>{ff.description}</label>
+          <div className="col-md-10">
+            <input className="form-control" id="poloniexApiKey" type="text" name={ff.name} placeholder={ff.placeholder} required />
+          </div>
         </div>
-      </div>
       )
     })
 
@@ -72,14 +83,14 @@ export default class AddAccount extends React.Component {
   render() {
     var slides = [<CarouselItem key='1' src=''>
       <div>
-        { this.accountForms.map((form, i) => {
-            return (<div key={i} className="col-md-12 account-type-selection">
-              <Button block={true} size="lg" color="light"
-                onClick={()=>{
-                  this.setState({currentAccountForm: form.service, activeSlide: 1});
-                }}>{form.service}</Button>
-            </div>)
-          }) }
+        {this.accountForms.map((form, i) => {
+          return (<div key={i} className="col-md-12 account-type-selection">
+            <Button block={true} size="lg" color="light"
+              onClick={() => {
+                this.setState({ currentAccountForm: form.service, activeSlide: 1 });
+              }}>{form.service}</Button>
+          </div>)
+        })}
       </div>
     </CarouselItem>,
     <CarouselItem key='2' src=''>
@@ -87,27 +98,45 @@ export default class AddAccount extends React.Component {
     </CarouselItem>
     ]
 
+    var accountButton = null;
+    if (this.state.activeSlide == 1) {
+      switch (this.state.submissionStatus) {
+        case 'none':
+          accountButton = <Button onClick={this.submitAccountForm} block={true} color="primary">Add Account</Button>
+          break
+        case 'loading':
+          accountButton = <Button block={true} color="primary"><i className="fa fa-circle-o-notch fa-spin"></i> Adding</Button>
+          break
+        case 'failure':
+          accountButton = <Button block={true} color="danger"><i className="fa fa-cross"></i> Failed</Button>
+          break
+        case 'success':
+          accountButton = <Button block={true} color="success"><i className="fa fa-check"></i> Success</Button>
+          break
+      }
+    }
+
     return (
       <div className="col-md-1" style={{ padding: 0 }}>
-        <Button block={true} onClick={this.toggle}>Add</Button>
+        <Button block={true} onClick={this.toggleModal}>Add</Button>
 
-        <Modal className="add-account-modal" isOpen={this.state.showModal} size="lg" toggle={this.toggle}>
+        <Modal className="add-account-modal" isOpen={this.state.showModal} size="lg" toggle={this.toggleModal}>
           <div className="modal-header">
             <h2 className="modal-title">Add Account</h2>
           </div>
           <div className="modal-body">
 
-            <Carousel activeIndex={this.state.activeSlide} previous={() => {}} next={() => { }}>
+            <Carousel activeIndex={this.state.activeSlide} previous={() => { }} next={() => { }}>
               {slides}
             </Carousel>
 
           </div>
           <div className="modal-footer">
-              { this.state.activeSlide == 1 ? <Button color="secondary" onClick={_ => {this.goToSlide(0)}}>Back</Button> : null }
-              <div className="col-md-6"></div>
-            { this.state.activeSlide == 1 ? <Button block={true} color="primary">Add Account</Button> : null }
-            <Button color="secondary" onClick={this.toggle}>Close</Button>
-            </div>
+            {this.state.activeSlide == 1 ? <Button color="secondary" onClick={_ => { this.goToSlide(0) }}>Back</Button> : null}
+            <div className="col-md-6"></div>
+            { accountButton }
+            <Button color="secondary" onClick={this.toggleModal}>Close</Button>
+          </div>
         </Modal>
       </div>
     )
