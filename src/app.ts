dee6da1c556ec88400cf366c8aa96f4b00635fb2
Jonathan Balls <jonathanballs@protonmail.com>
2017-10-28 21:27:42 +0100

    Moved portfolio urls to separate router

--- src/app.ts
+++ src/app.ts
@@ -20,6 +20,7 @@ import { User, Price } from "./models";
 import { loginRequired } from './auth/auth'
 import settingsRouter from './settings/settings'
 import authRouter from './auth/auth'
+import portfolioRouter from './portfolio/portfolio'
 
 mongoose.connect('mongodb://db/polytrader', {useMongoClient: true});
 
@@ -80,6 +81,7 @@ app.use(expressValidator())
 
 app.use('/account', settingsRouter)
 app.use('/auth', authRouter)
+app.use('/portfolio', portfolioRouter)
 
 app.get('/', (req, res) => {
     if (req.user) {
@@ -89,90 +91,6 @@ app.get('/', (req, res) => {
     else {
         res.render('index')
     }
-});
-
-// Get poloniex data
-app.get('/portfolio', loginRequired, (req, res) => {
-    //Redirect to account in case of API key not setup
-    if(!req.user.accounts.length) {
-        res.redirect('/account')
-    }
-
-    var accountService = req.user.accounts[0].service
-    var wrapper = servicesList.filter(s => s.key == accountService)[0]
-
-    var p = new wrapper.wrapper(wrapper.serverAuth, req.user.accounts[0].userAuth)
-
-    p.returnBalances().then(balances => {
-        p.returnPortfolioHistory().then(portfolioHistory => {
-
-            // Find list of all currency pairs
-            var all_pairs = new Set()
-            portfolioHistory.map(p => {
-                p.balances.map(b => { all_pairs.add("BTC_" + b.currency) })
-            })
-
-            // Fetch historic prices from database
-            let prices = []
-            Price.aggregate([
-                {$match: {
-                        'currency_pair': {$in : Array.from(all_pairs)},
-                        'date': {$gt : portfolioHistory[0].timestamp}
-                }},
-                {$project: {
-                    yearMonthDay: { $dateToString: { format: "%Y-%m-%d", date: "$date" } },
-                    currency_pair: "$currency_pair",
-                    daily_average: "$daily_average"
-                }},
-                {$group: {
-                    _id: "$yearMonthDay",
-                    prices: { $push: { currency_pair: "$currency_pair", daily_average: "$daily_average"}}
-                }},
-                {$sort: {
-                    _id: 1
-                }}
-            ]).cursor({}).exec()
-            .on('data', doc => prices.push(doc))
-            .on('end', _ => {
-
-                // Helper function to get the user's portfolio at a certain time
-                var portfolioAtTime = (time: Date) => {
-                    return portfolioHistory.filter(p => p.timestamp < time )
-                        .sort((a, b) => b.timestamp.getTime() - a.timestamp.getTime())[0]
-                }
-
-                var portfolioHistoryProcessed = prices.map(price => {
-                    // Get users portfolio on date
-                    var pd_sp = price._id.split('-')
-                    var price_date = new Date(pd_sp[0], pd_sp[1]-1, pd_sp[2])
-                    var p = clone(portfolioAtTime(price_date))
-                    p.timestamp = price_date
-
-                    p.balances.forEach(b => {
-                        if (b.currency == "BTC") {
-                            b.btcValue = b.amount
-                            return;
-                        }
-                        else if(b.currency == "USDT") {
-                            b.btcValue = "0.0"
-                            return;
-                        }
-
-                        var b_price = price.prices.filter(p => p.currency_pair == "BTC_" + b.currency)[0]
-
-                        if(typeof(b_price) != 'undefined') {
-                            b.btcValue = new Big(b_price.daily_average).times(b.amount).toFixed(10)
-                        } else {
-                            b.btcValue = '0.0'
-                        }
-                    })
-                    return p
-                })
-
-                res.render('portfolio/portfolio', {balances, portfolioHistory: portfolioHistoryProcessed});
-            })
-        }).catch(err => res.render('portfolio/portfolio', {err}))
-    }).catch(err => res.render('portfolio/portfolio', {err}))
 });
 
 app.use((req, res, next) => {
