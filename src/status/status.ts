7cd8d24204f4c1c8bd7e63dd25b41881bc0427cb
Jonathan Balls <jonathanballs@protonmail.com>
2017-11-10 16:03:39 +0000

    API status page is good

--- src/status/status.ts
+++ src/status/status.ts
@@ -2,13 +2,13 @@ import * as express from 'express';
 import { loginRequired, loginRequiredApi } from '../auth/auth'
 import UserModel from '../models/user'
 import * as mongoose from 'mongoose'
-import services from '../wrappers/services'
 import { servicesClient } from '../wrappers/services'
+import services from '../wrappers/services'
 import * as multiparty from 'multiparty'
 
 var router = express.Router()
 export default router
 
 router.get('/', (req, res) => {
-    res.render('status/status')
+    res.render('status/status', { services })
 })
