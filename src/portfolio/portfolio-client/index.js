eb14211a099ea820764e770e8688ccff8213bdfc
Jonathan Balls <jonathanballs@protonmail.com>
2017-11-11 16:35:22 +0000

    Added loading spinner to portfolio page

--- src/portfolio/portfolio-client/index.js
+++ src/portfolio/portfolio-client/index.js
@@ -99,7 +99,12 @@ class App extends React.Component {
   render() {
 
     if (!this.state.portfolioHistories) {
-      return <h1>Loading...</h1>
+      return <div className="row justify-content-center">
+      <div className="col-md-1"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" className="lds-rolling">
+        <circle cx="50" cy="50" fill="none" ng-attr-stroke="{{config.color}}" ng-attr-stroke-width="{{config.width}}" ng-attr-r="{{config.radius}}" ng-attr-stroke-dasharray="{{config.dasharray}}" stroke="#1d3f72" strokeWidth="5" r="35" strokeDasharray="164.93361431346415 56.97787143782138" transform="rotate(8 50 50)">
+          <animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;360 50 50" keyTimes="0;1" dur="1.5s" begin="0s" repeatCount="indefinite"></animateTransform>
+        </circle>
+      </svg></div></div>
     }
 
     var currentBalances = this.mergePortfolios(this.getPortfoliosAtTime(new Date))
