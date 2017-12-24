4c485669d82a2dcff867a4f5b14b16c533b7b78a
Jonathan Balls <jonathanballs@protonmail.com>
2017-12-24 09:51:08 +0000

    Add commit hash and time to status page

--- src/status/status.ts
+++ src/status/status.ts
@@ -1,6 +1,8 @@
+import * as child_process from "child_process";
 import * as express from "express";
 import * as mongoose from "mongoose";
 import * as multiparty from "multiparty";
+import * as process from "process";
 import { loginRequired, loginRequiredApi } from "../auth/auth";
 import PriceModel from "../models/price";
 import UserModel from "../models/user";
@@ -10,9 +12,17 @@ import services from "../wrappers/services";
 const router = express.Router();
 export default router;
 
+function execute(command, callback) {
+    child_process.exec(command, (error, stdout, stderr) => { callback(stdout); });
+}
+
 router.get("/", (req, res) => {
     // Get currency information
     PriceModel.getCurrencyStats().then((currencies) => {
-        res.render("status/status", { currencies, services });
+        execute("git rev-parse HEAD", (commitHash) => {
+            execute("git --no-pager log -1 --format=%cd", (commitTime) => {
+                res.render("status/status", { currencies, services, commitHash, commitTime });
+            });
+        });
     });
 });
