23e2acc74c514c1d60c78249c158814e4e0790ad
Jonathan Balls <jonathanballs@protonmail.com>
2018-01-09 13:22:34 +0000

    Add a favicon

--- src/app.ts
+++ src/app.ts
@@ -117,6 +117,10 @@ app.get("/", (req, res) => {
     }
 });
 
+app.get("/favicon.ico", (req, res) => {
+    res.sendFile("./static/images/favicon.ico", { root: "./" });
+});
+
 app.use((req, res, next) => {
     res.status(404).render("404"); // 404 handler
 });
